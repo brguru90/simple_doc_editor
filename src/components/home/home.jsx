c8c136e02ade9e2012601e962bd0f4db09bcc2c9
Guruprasad BR <brguru90@gmail.com>
2021-06-27 00:25:52 +0530

    simple doc editor

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -3,12 +3,22 @@ import { Link } from "react-router-dom"
 
 import "./style.scss"
 
+import page1 from "../../assets/page1.jpg"
+import page2 from "../../assets/page2.jpg"
+import page3 from "../../assets/page3.jpg"
+import page4 from "../../assets/page4.jpg"
+
 export default class home extends Component {
   
 
     render() {
         return (
            <div className="home">
+               <div className="resume_heading">Resume</div>
+              <img src={page1} className="resume" />
+              <img src={page2}  className="resume" />
+              <img src={page3}  className="resume" />
+              <img src={page4}  className="resume" />
               
            </div>
         )
